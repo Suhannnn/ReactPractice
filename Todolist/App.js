de11006c3502accd9ddb82d69846b8f649afba61
Suhannnn <liusuhan1994@gmail.com>
2021-09-17 23:16:18 +1000

    Todolist

---
+++ Todolist/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import TodoList from "./Components/TodoList";
+import TodoForm from "./Components/TodoForm";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: ["Finish homework", "Wash dishes", "Clean room", "Make waffles"],
+    };
+  }
+
+  addTodo = (todo) => {
+    this.setState((prevState) => ({
+      todos: [...prevState.todos, todo],
+    }));
+  };
+
+  deleteTodo = (value) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo !== value),
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Todos</h1>
+        <TodoList todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <TodoForm addTodo={this.addTodo} />
+      </div>
+    );
+  }
+}
+
+export default App;
